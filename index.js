7276ba4328e4dc264639d78c739c9299a81675e6
Matheus Maximiliano <maximiliano.mprado@gmail.com>
2023-10-30 14:23:54 -0300

    feat: :art: jenkins add

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const http = require('http');
 const os = require('os');
 
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.writeHead(200);
+    res.setHeader('Content-Type', 'text/plain');
+    res.writeHead(200);
 
-  // Obtém o nome do pod usando o módulo 'os'
-  const podName = os.hostname();
+    // Obtém o nome do pod usando o módulo 'os'
+    const podName = os.hostname();
 
-  res.end(`Pod rodando: ${podName}\n`);
+    res.end(`Pod rodando First Commit Jenkins: ${podName}\n`);
 });
 
 const port = 3000;
 server.listen(port, () => {
-  console.log(`Servidor Node.js está rodando na porta ${port}`);
+    console.log(`Servidor Node.js está rodando na porta ${port}`);
 });

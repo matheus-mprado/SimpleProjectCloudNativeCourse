48bd79c987d1dfb186dabe2edfbe742a5aa7c9dd
Matheus Maximiliano <maximiliano.mprado@gmail.com>
2023-10-30 14:19:54 -0300

    feat: :art: first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const os = require('os');
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.writeHead(200);
+
+  // Obtém o nome do pod usando o módulo 'os'
+  const podName = os.hostname();
+
+  res.end(`Pod rodando: ${podName}\n`);
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Servidor Node.js está rodando na porta ${port}`);
+});
